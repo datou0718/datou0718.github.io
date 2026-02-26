d00a83bfea7c3cf5d8e1b9dab1686db03ee2786e
Datou0718 <lyck92@gmail.com>
2026-02-26 12:34:51 +0800

    feat: dynamically inject homepage title and meta description

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { content } from './src/data/content'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        const rawName = content.name.split(' (')[0];
+        return html
+          .replace('__TITLE__', `${rawName}'s Homepage`)
+          .replace('__DESCRIPTION__', content.title.replace(/"/g, '&quot;'));
+      },
+    }
+  ],
   base: '/',
 })
